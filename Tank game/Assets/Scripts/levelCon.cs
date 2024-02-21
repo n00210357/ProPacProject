d312e6a5ac186e11a3c77d7910f634e2de630993
n00210357 <99271838+n00210357@users.noreply.github.com>
2024-02-21 13:17:56 +0000

    seventeeth (navmesh)

--- Tank game/Assets/Scripts/levelCon.cs
+++ Tank game/Assets/Scripts/levelCon.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class levelCon : MonoBehaviour
 {
+    public NavMeshSurface sur;
+
     //the enemy spawn points 
     public Transform[] spawnPoint;
 
     //the maps patrol points
     public Transform[] patrolPoint;
 
-    //detect what enemies are alive
-    public GameObject[] spawnedEnemies;
-
     //what enemies can be spawned
     public GameObject[] enemiesToSpawn;
 
     //enemies choice of gun
     public GameObject[] enemyGuns;
+
+    //detect what enemies are alive
+    public GameObject[] spawnedEnemies;
+
+    void Start()
+    {
+        sur.BuildNavMesh();
+
+        if (spawnPoint[0] == null)
+        {
+            spawnPoint = new Transform[patrolPoint.Length];
+
+            for (int i = 0; i <= patrolPoint.Length - 1; i++)
+            {
+                spawnPoint[i] = patrolPoint[i];
+            }
+        }
+
+
+    }
 }
